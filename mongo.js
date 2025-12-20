e24346080263170b26c25a8585310f7838f6ad60
arshviuef <201494334+arshvuief@users.noreply.github.com>
2025-12-20 15:15:37 +0000

    uibuild

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${process.env.PASS}@cluster0.a7b3qf9.mongodb.net/noteApp?appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "STAYC!!!",
+  important: true,
+});
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close;
+});
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
