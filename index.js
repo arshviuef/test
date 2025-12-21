77b3c232492aa9bff44b10929939ee0eac825ea2
arshviuef <201494334+arshvuief@users.noreply.github.com>
2025-12-21 12:17:42 +0000

    uibuild

--- index.js
+++ index.js
@@ -3,120 +3,95 @@ const morgan = require("morgan");
 const Note = require("./models/person");
 const app = express();
 
-morgan.token("request", (request, response) => {
-  return JSON.stringify(request.body);
-});
-
 app.use(express.static("dist"));
 app.use(express.json());
-app.use(
-  morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      tokens["response-time"](req, res),
-      "ms",
-      tokens.request(req, res),
-    ].join(" ");
-  })
-);
+app.use(morgan("tiny"));
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/api/persons", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+app.get("/api/persons", (request, response, next) => {
+  Note.find({})
+    .then((notes) => {
+      response.json(notes);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/info", (request, response) => {
-  const currentDate = new Date();
-  const currentLength = persons.length;
-
-  response.send(
-    `<h2>Phonebook has info for ${currentLength} people</h2><h2>${currentDate}</h2>`
-  );
+app.get("/info", (request, response, next) => {
+  Note.countDocuments({})
+    .then((result) => {
+      const currentDate = new Date();
+      response.send(`<h2>Phonebook has info for ${result} people</h2>
+      <h2>${currentDate}</h2>`);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
+app.get("/api/persons/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((result) => {
+      if (!result) {
+        return response.status(404).end();
+      }
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+      response.json(result);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
+app.delete("/api/persons/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      if (!result) {
+        return response.status(404).json({ error: "not found" });
+      }
 
-  response.status(204).end();
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  return String(Math.floor(Math.random() * 100000) + 1);
-};
-
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
-  const exists = persons.find((person) => person.name === body.name);
+  Note.findOne({ name: body.name }).then((result) => {
+    if (result) {
+      return response.status(409).json({ error: "name must be unique" });
+    }
+    if (!body.name) {
+      return response.status(400).json({ error: "missing name" });
+    }
+    if (!body.number) {
+      return response.status(400).json({ error: "missing number" });
+    }
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: "name missing",
+    const person = new Note({
+      name: body.name,
+      number: body.number,
     });
-  } else if (!body.number) {
-    return response.status(400).json({
-      error: "number missing",
-    });
-  } else if (exists) {
-    return response.status(409).json({
-      error: "name must be unique",
-    });
+
+    person
+      .save()
+      .then((savedNote) => {
+        response.json(savedNote);
+      })
+      .catch((error) => next(error));
+  });
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).json({ error: "malformatted id" });
   }
 
-  const person = new Note({
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  });
+  if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
 
-  person
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => {
-      console.log("Error saving note", error.message);
-    });
-});
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
